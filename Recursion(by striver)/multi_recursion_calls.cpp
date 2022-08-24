80fba8ca9001ebcab85151fcf1419993853c0229
RitikKumar202 <ritik2002july@gmail.com>
2022-08-25 00:51:22 +0530

    Recursion L-5

---
+++ Recursion(by striver)/multi_recursion_calls.cpp
@@ -0,0 +1,19 @@
+//Multiple Recursion Calls
+// Solved using Fibonacci Series(nth fibonacci number)
+// Time complex = exponential 2^n(near about)
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int n){
+    if(n<=1) return n;
+
+    int last = func(n-1);
+    int second_last = func(n-2);
+
+    return last + second_last;
+}
+
+int main(){
+    int n=4;
+    cout << func(n);
+}
